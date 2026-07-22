8f59fbeaa6192b5e8f2712a137ac72267bad347d
grobertson-png <g.robertson@tmtsolutions.com>
2026-07-22 09:38:42 -0500

    should work maybe

--- AxisIntegrationVB/CameraView.vb
+++ AxisIntegrationVB/CameraView.vb
@@ -1,5 +1,11 @@
 ﻿Imports FFmpeg.AutoGen
 
+'-----------------------
+Imports LibVLCSharp.Shared
+Imports LibVLCSharp.WinForms
+
+'----------------
+
 Public Class CameraView
     Private connectionString As String = "server=localhost;user=root;password=TMT$olutions;database=vnc_view_schema.list_device;"
     'Private ReadOnly Cams As New List(Of AxAXISMEDIACONTROLLib.AxAxisMediaControl)
@@ -29,10 +35,33 @@ Public Class CameraView
 
     Private Sub CameraView_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
         'EndCall()
+
+        '------------------------ Glade
+
+        'If _mediaPlayer IsNot Nothing Then
+        '_mediaPlayer.Dispose()
+        'End If
+
+        'If _libVLC IsNot Nothing Then
+        '_libVLC.Dispose()
+        'End If
+
+        '------------------------- Glade
     End Sub
+
+    '----------------------- Glade
+    'Private _libVLC As LibVLC
+    'Private _mediaPlayer As MediaPlayer
+    '------------------------- Glade
 
     Private Sub CameraView_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.KeyPreview = True
+
+        'New code from Glade ----------------
+        'Core.Initialize()
+        '_libVLC = New LibVLC()
+        '_mediaPlayer = New MediaPlayer(_libVLC)
+        'VideoView1.MediaPlayer = _mediaPlayer
     End Sub
 
     Private Sub CameraView_Shown(sender As Object, e As EventArgs) Handles Me.Shown
@@ -51,6 +80,8 @@ Public Class CameraView
             MessageBox.Show($"Audio On: {audioSwitch}")
         End If
     End Sub
+
+
 
 
     'rtsp://<willTestCam>:<root>@<host>/axis-media/media.amp
@@ -97,19 +128,55 @@ Public Class CameraView
 
     'Private Sub EndCall()
     '    Cams(0).AudioTransmitStop()
-    'End Subgit 
+    'End Sub
+
+    '-----------------------------------------------------------------------------------------------------------------
+    'New code from Glade to make the video appear
+
+
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+
+        Dim username As String = "willTestCam"
+        Dim password As String = "root"
+        Dim ipAddress As String = "192.168.0.208"
+        Dim cameraUrl As String = "rtsp://" & username & ":" & password & "@" & ipAddress & "/axis-media/media.amp?videocodec=h264&camera=1&resolution=640x480"
+
+        Using media As New Media(_libVLC, New Uri(cameraUrl))
+            _mediaPlayer.Play(media)
+        End Using
+
+    End Sub
+
+    'for when we integrate not needing to press a button
+    Private Sub camPlay(user As String, passW As String, ip As String)
+
+        Dim username As String = user
+        Dim password As String = passW
+        Dim ipAddress As String = ip
+        Dim cameraUrl As String = "rtsp://" & username & ":" & password & "@" & ipAddress & "/axis-media/media.amp?videocodec=h264&camera=1&resolution=640x480"
+
+        Using media As New Media(_libVLC, New Uri(cameraUrl))
+            _mediaPlayer.Play(media)
+        End Using
+    End Sub
 
 
     Public Sub Video_Init()
-
+        'Core.Initialize()
+        '_libVLC = New LibVLC()
+        '_mediaPlayer = New MediaPlayer(_libVLC)
         'Dim newAMC As AxAXISMEDIACONTROLLib.AxAxisMediaControl = Globals.CreateCtrl(Of AxAXISMEDIACONTROLLib.AxAxisMediaControl)(CameraPanel)
 
+        'Dim viewFeed As VideoView = Globals.CreateCtrl(Of VideoView)(CameraPanel)
+        'viewFeed.MediaPlayer = _mediaPlayer
+
+        'camPlay("willTestCam", "root", "192.168.0.208")
+
         Dim subForm As VideoFeed = Globals.CreateCtrl(Of VideoFeed)(CameraPanel)
+        subForm.TopLevel = False
         subForm.Show()
 
 
     End Sub
-
-
 
 End Class
